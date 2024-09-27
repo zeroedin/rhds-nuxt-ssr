eedcd844f00afe36e961d6814a55ed4d7a5b81ff
Steven Spriggs <steven.spriggs@gmail.com>
2024-09-27 17:07:39 -0400

    update config and styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,46 +16,8 @@ export default defineNuxtConfig({
       'rh-tabs', 
       'rh-tab', 
       'rh-tab-panel',
-      'rh-subnav'
+      'rh-subnav',
+      'rh-switch',
     ]
   }]],
-  vite: {
-    build: {
-      target: 'esnext',
-    },
-  },
-  hooks: {
-    'vite:extendConfig'(config, { isServer }) {
-      console.log(
-        isServer ? 'server' : 'client',
-        'config target is',
-        config.build!.target
-      );
-    },
-  },
-  nitro: {
-    esbuild: {
-      options: {
-        target: 'esnext'
-      },
-    },
-    // moduleSideEffects: [
-    //   '@rhds/elements/rh-alert/rh-alert.js',
-    //   '@rhds/elements/rh-button/rh-button.js',
-    //   '@rhds/elements/rh-cta/rh-cta.js',
-    //   '@rhds/elements/rh-card/rh-card.js',
-    //   '@rhds/elements/rh-pagination/rh-pagination.js',
-    //   '@rhds/elements/rh-tabs/rh-tabs.js',
-    //   '@rhds/elements/rh-tag/rh-tag.js',
-    //   '@rhds/elements/rh-tile/rh-tile.js',
-    //   '@rhds/elements/rh-icon/rh-icon.js',
-    //   '@rhds/icons/microns/arrow-right.js',
-    //   '@rhds/icons/ui/arrow-right.js',
-    // ]
-  },
-  vue: {
-    compilerOptions: {
-      isCustomElement: (tag) => tag.includes("-"),
-    },
-  },
 })
