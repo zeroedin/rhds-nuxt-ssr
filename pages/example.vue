d0ebca2182d38716415f69c101cc387ae1a0b851
Kyle Buchanan <kylebuch8@gmail.com>
2024-09-27 13:48:40 -0400

    adding content to example page

--- pages/example.vue
+++ pages/example.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Example</h1>
+  <h2>Popular products</h2>
+  <div>
+    <rh-card>
+      <div slot="header">
+        <h3>Red Hat Enterprise Linux</h3>
+      </div>
+    </rh-card>
+  </div>
 </template>
+
+<script type="module">
+import "@rhds/elements/rh-card/rh-card.js";
+</script>
+
+<script setup>
+useHead({
+  script: [
+    {
+      type: 'module',
+      innerHTML: `
+        import '/javascript/dsd-polyfill.js';
+      `
+    },
+  ],
+});
+</script>
